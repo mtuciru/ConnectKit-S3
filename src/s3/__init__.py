9e0532f218804c410bcef07a12820621951f4365
RealMetamorph <andr.timchuk@yandex.ru>
2025-03-29 21:02:42 +0300

    add __all__ in init files

--- src/s3/__init__.py
+++ src/s3/__init__.py
@@ -4,5 +4,8 @@ from s3.s3_connector import s3, S3
 
 try:
     from s3 import asyncio
+
+    __all__ = ["s3", "S3", "asyncio", "settings"]
 except ImportError:
+    __all__ = ["s3", "S3", "settings"]
     pass
--- src/s3/asyncio/__init__.py
+++ src/s3/asyncio/__init__.py
@@ -1,4 +1,6 @@
 try:
     from s3.asyncio.s3_connector import async_s3, AsyncS3, AsyncS3Instance
+
+    __all__ = ["async_s3", "AsyncS3", "AsyncS3Instance"]
 except ImportError:
     raise ImportError("Async functions disabled. Please install extra 'async'")
